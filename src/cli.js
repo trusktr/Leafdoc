365cd622e1cd283eda72b7d510cd633c09827c5c
Joe Pea <joe@trusktr.io>
2019-08-24 23:34:36 -0700

    compile TypeScript to CommonJS format (for runnning in current versions of Node.js)

--- src/cli.js
+++ src/cli.js
@@ -9,7 +9,13 @@ require('ts-node').register({
 	// manually supply our own compilerOptions, otherwise if we run this file
 	// from another project's location (f.e. from Saffron) then ts-node will use
 	// the compilerOptions from that other location, which may not work.
-	compilerOptions: require('../tsconfig.json').compilerOptions,
+	compilerOptions: {
+        ...require('../tsconfig.json').compilerOptions,
+
+        // so that it runs in Node.js (ES Modules aren't released yet at time of
+        // writing)
+        module: 'commonjs',
+    },
 })
 
 require('./cli.ts')
